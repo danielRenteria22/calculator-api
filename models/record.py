e91f5313b5af51180714995a513df14f34bc297c
Daniel Renteria <jose.r.renteria@oracle.com>
2023-02-16 02:32:13 -0600

    Added missing fields to record

--- models/record.py
+++ models/record.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer,Enum,Double,Column, String, ForeignKey
+from sqlalchemy import Integer,Enum,Double,Column, String, ForeignKey, Date
 from sqlalchemy.orm import relationship, mapped_column
 
 from main import db
@@ -7,6 +7,9 @@ class Record(db.Model):
     id = Column(Integer, primary_key=True)
     operation_id = mapped_column(ForeignKey('operation.id'))
     user_id = mapped_column(ForeignKey('user.id'))
+    amount = Column(Integer)
+    user_balance = Column(Integer)
+    operation_response = Column(Integer)
     
     operation = relationship('Operation',back_populates='children')
     user = relationship('User',back_populates='children')
